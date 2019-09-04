6c7b937d5979682fb7de9aad1a87b00c92b2d761
Nadav Gordon <nadavg@omc.co.il>
2019-09-04 18:19:32 +0300

    mod: skip cwm if no config file found mod: consolidated cwm global params mod: improved cwm checks mod: better function organization fix: install continues when cwm api key missing fix: waitorstop function wont stop on piping fix: modified IFS not set back to default fix: some global params were not exported fix: cleanup

--- include/startup-cwm.sh
+++ include/startup-cwm.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
+# skip cwm related steps if config file not found
 if [ ! -f "$CWMCONFIGFILE" ]; then
-    echo "Missing CWM config file. Exiting."
-    exit 1
+    echo "Missing CWM config file."
+    # exit 1
+    return 0
 fi
 
+# parse cwm config into global params
 CONFIG=`cat $CWMCONFIGFILE`
-
+STD_IFS=$IFS
 IFS=$'\n'
-
 for d in $CONFIG; do
 
-    export `echo $d | cut -f 1 -d"="`="`echo $d | cut -f 2 -d"="`"
+    key=$(echo $d | cut -f1 -d"=")
+    value=$(echo $d | cut -f2 -d"=")
+    export "CWM_${key^^}"="$value"
 
 done
+IFS=$STD_IFS
 
-CWMSITE=$url
-ADMINEMAIL=$email
-ADMINPASSWORD="$password"
-APICLIENTID=$apiClientId
-APISECRET=$apiSecret
-VMNAME=$name
-ZONE=$zone
-CPU=$cpu
-RAM=$ram
-DISKSIZE=$disk0size
-WANNICIDS=`cat $CWMCONFIGFILE | grep ^vlan.*=wan-.* | cut -f 1 -d"=" | cut -f 2 -d"n"`
-LANNICIDS=`cat $CWMCONFIGFILE | grep ^vlan.*=lan-.* | cut -f 1 -d"=" | cut -f 2 -d"n"`
-DISKS=`cat $CWMCONFIGFILE | grep ^disk.*size=.* | wc -l`
-UUID=$(cat /sys/class/dmi/id/product_serial | cut -d '-' -f 2,3 | tr -d ' -' | sed 's/./&-/20;s/./&-/16;s/./&-/12;s/./&-/8')
+# additional cwm global params
+export ADMINEMAIL=$CWM_EMAIL
+export ADMINPASSWORD="$CWM_PASSWORD"
+export CWM_WANNICIDS=`cat $CWMCONFIGFILE | grep ^vlan.*=wan-.* | cut -f 1 -d"=" | cut -f 2 -d"n"`
+export CWM_LANNICIDS=`cat $CWMCONFIGFILE | grep ^vlan.*=lan-.* | cut -f 1 -d"=" | cut -f 2 -d"n"`
+# DISKS=`cat $CWMCONFIGFILE | grep ^disk.*size=.* | wc -l`
+export CWM_UUID=$(cat /sys/class/dmi/id/product_serial | cut -d '-' -f 2,3 | tr -d ' -' | sed 's/./&-/20;s/./&-/16;s/./&-/12;s/./&-/8')
 
 var=0
-for nicid in $WANNICIDS; do
+for nicid in $CWM_WANNICIDS; do
 
     var=$((var+1))
     nicvar=ip${nicid}
-    export `echo WANIP$var`=`echo ${!nicvar}`
+    export `echo CWM_WANIP$var`=`echo ${!nicvar}`
     unset nicvar
 
 done
 
 var=0
-for nicid in $LANNICIDS; do
+for nicid in $CWM_LANNICIDS; do
 
     var=$((var+1))
     nicvar=ip${nicid}
-    export `echo LANIP$var`=`echo ${!nicvar}`
+    export `echo CWM_LANIP$var`=`echo ${!nicvar}`
     unset nicvar
 
 done
 
-if [[ -z "$APICLIENTID" || -z "$APISECRET" ]]; then
+# fail install if cwm api key or secret is missing
+if [[ -z "$CWM_APICLIENTID" || -z "$CWM_APISECRET" ]]; then
 
     echo "No CWM API Client ID or Secret is set. Exiting."
     exit 1
@@ -58,31 +57,31 @@ if [[ -z "$APICLIENTID" || -z "$APISECRET" ]]; then
 fi
 
 # Function: updateServerDescription
-# Purpose: Update CWM Server's Overview->Descriptoin text field.
+# Purpose: Update CWM Server's Overview->Description text field.
 # Usage: updateServerDescription "Some kind of description"
 
 function updateServerDescription() {
 
-    curl --location -f -X PUT --retry-connrefused --retry 3 --retry-delay 2 -H "AuthClientId: ${APICLIENTID}" -H "AuthSecret: ${APISECRET}"  "https://$CWMSITE/svc/server/$UUID/description" --data-urlencode $'description='"$1"
+    curl --location -f -X PUT --retry-connrefused --retry 3 --retry-delay 2 -H "AuthClientId: ${CWM_APICLIENTID}" -H "AuthSecret: ${CWM_APISECRET}"  "https://$CWM_URL/svc/server/$CWM_UUID/description" --data-urlencode $'description='"$1"
 
-    errorCode=$?
-    if [ $errorCode -ne 0 ]; then
+    local exitCode=$?
+    if [ $exitCode -ne 0 ]; then
 
         echo "Error updating server description" | log
         return 1
 
     fi
 
-    echo "Updated Overview->Description data for $UUID" | log
+    echo "Updated Overview->Description data for $CWM_UUID" | log
 
 }
 
 function getServerDescription() {
 
-    description=`curl --location -f --retry-connrefused --retry 3 --retry-delay 2 -H "AuthClientId: ${APICLIENTID}" -H "AuthSecret: ${APISECRET}" "https://$CWMSITE/svc/server/$UUID/overview" | grep -Po '(?<="description":")(.*?)(?=",")'`
+    description=`curl --location -f --retry-connrefused --retry 3 --retry-delay 2 -H "AuthClientId: ${CWM_APICLIENTID}" -H "AuthSecret: ${CWM_APISECRET}" "https://$CWM_URL/svc/server/$CWM_UUID/overview" | grep -Po '(?<="description":")(.*?)(?=",")'`
     
-    errorCode=$?
-    if [ $errorCode -ne 0 ]; then
+    local exitCode=$?
+    if [ $exitCode -ne 0 ]; then
 
         echo "Error retrieving server overview"
         return 1
@@ -144,18 +143,18 @@ function getServerIP() {
     
     IPS=`cat $CWMCONFIGFILE | grep ^ip.*=* | cut -f 2 -d"i" | cut -f 2 -d"p"`
 
-    if [ ! -z "$WANNICIDS" ]; then
+    if [ ! -z "$CWM_WANNICIDS" ]; then
 
-        index=`echo $WANNICIDS | awk '{print $1;}'`
+        index=`echo $CWM_WANNICIDS | awk '{print $1;}'`
         index=$((index+1))
         echo $IPS | awk -v a="$index" '{print $a;}' | cut -f 2 -d"="
         return 0
 
     fi
 
-    if [ ! -z "$LANNICIDS" ]; then
+    if [ ! -z "$CWM_LANNICIDS" ]; then
 
-        index=`echo $LANNICIDS | awk '{print $1;}'`
+        index=`echo $CWM_LANNICIDS | awk '{print $1;}'`
         index=$((index+1))
         echo $IPS | awk -v a="$index" '{print $a;}' | cut -f 2 -d"="
         return 0
@@ -174,170 +173,6 @@ function getServerIPAll() {
     fi
         
     echo `cat $CWMCONFIGFILE | grep ^ip.*=* | cut -f 2 -d"="`
-
-}
-
-function join_by {
-
-    local IFS="$1"; shift; echo "$*";
-
-}
-
-function createSwapFile() {
-
-    # 1:filename, 2:megabytes, 3:path
-
-    # if path is given, know how to handle it when creating swap
-    if [ ! -z $3 ]; then
-
-        if [ -d $3 ]; then
-
-            # path exists
-            createDir=0
-
-        else
-
-            # new path
-            createDir=1
-
-        fi
-
-    else
-
-        # path not given
-        createDir=2
-
-    fi
-
-    if [ -z $1 ]; then
-
-        # (>&2 echo "error: no filename given to swap file")
-        echo "error: no filename given to swap file" | log
-        return 1
-
-    fi
-
-    if [[ $createDir -eq 2 && -e $1 ]] || [[ $createDir -eq 0 && -e "$3/$1" ]]; then
-
-        echo "error: a file with this name already exists" | log
-        return 1
-
-    fi
-
-    if [ -z $2 ]; then
-
-        echo "error: swap size (in MB) must be provided" | log
-        return 1
-
-    fi
-
-    if [[ $2 =~ '^[0-9]+$' ]] || [[ $2 -le 0 ]]; then
-
-        echo "error: swap size must be a number greater than 0" | log
-        return 1
-
-    fi
-
-    diskSizeMb=`df --output=avail -m "$PWD" | sed '1d;s/[^0-9]//g'`
-    swapSizeAllowed=$((diskSizeMb/2))
-
-    if [ $2 -gt $swapSizeAllowed ]; then
-
-        echo "error: maximum swap size (in MB) can be $swapSizeAllowed" | log
-        return 1
-
-    fi
-
-    # create swap in existing directory
-    if [ $createDir -eq 0 ]; then
-
-        swapFile="$3/$1"
-
-    fi
-
-    # create new directory for swap
-    if [ $createDir -eq 1 ]; then
-
-        mkdir -p $3
-        swapFile="$3/$1"
-
-    fi
-
-    # create swap in current path
-    if [ $createDir -eq 2 ]; then
-
-        swapFile="$1"
-
-    fi
-
-    # generate swap file and mount it
-    dd if=/dev/zero of=$swapFile bs=1M count=$2
-    mkswap $swapFile >/dev/null || { echo 'mkswap failed' ; return 1; }
-    swapon $swapFile >/dev/null || { echo 'swapon failed' ; return 1; }
-    chmod 600 $swapFile >/dev/null || { echo 'chmod swapfile failed' ; return 1; }
-
-    if [ ! -e $swapFile ]; then
-
-        echo "error: did not complete swap creation properly"
-        return 1
-
-    fi
-
-    echo "$swapFile"
-    return 0
-
-}
-
-function removeSwapFile() {
-
-    # 1: filename given when created swap with createSwapFile()
-    if [ ! -e $1 ]; then
-
-        echo "error: a swapfile with this name was not found. did nothing" | log
-        return 1
-
-    fi
-
-    swapoff $1
-    rm -f $1
-
-    return 0
-
-}
-
-function curlDownload() {
-
-    checkPackageInstalled curl
-    
-    # check if url is given
-    if [ -z "$1" ]; then
-
-        echo "No download url is provided. Exiting (1)."
-        return 1
-        
-    fi
-
-    # allow for nameless and nameful downloads
-    if [ -z "$2" ]; then 
-
-        httpResponse=$(curl --fail --location --write-out %{http_code} --max-redirs 3 --retry 3 --retry-connrefused --retry-delay 2 --max-time 90 --url $1 --remote-name)
-        local exitCode=$?
-
-    else
-
-        httpResponse=$(curl --fail --location --write-out %{http_code} --max-redirs 3 --retry 3 --retry-connrefused --retry-delay 2 --max-time 90 --url $1 --output $2)
-        local exitCode=$?
-
-    fi
-
-    if [ "$exitCode" != "0" ] || [ "$httpResponse" != "200" ]; then
-
-        echo "Download failed with exitCode:$exitCode and httpResponse:$httpResponse"
-        return 1
-        
-    fi
-
-    return 0
 
 }
 
@@ -368,4 +203,4 @@ function apt () {
     fi
 }
 
-SERVERIP="$(getServerIP)"
+CWM_SERVERIP="$(getServerIP)"
--- include/startup.sh
+++ include/startup.sh
@@ -220,10 +220,11 @@ function backupFile() {
 
 function waitOrStop() {
 
-    exitCode=$?
+    # exitCode=$?
+    exitCode=${PIPESTATUS[0]}
     waitExitCode=$1
 
-    if [ "$waitExitCode" != "$exitCode" ]; then
+    if [ $waitExitCode -ne $exitCode ]; then
 
 	    echo "Waiting for $waitExitCode. Execution return $exitCode. exiting (1)" | log
         exit 1;
@@ -248,6 +249,164 @@ function checkPackageInstalled() {
     fi
 
     unset package
+
+}
+
+function curlDownload() {
+
+    checkPackageInstalled curl
+    
+    # check if url is given
+    if [ -z "$1" ]; then
+
+        echo "No download url is provided. Exiting (1)."
+        return 1
+        
+    fi
+
+    # allow for nameless and nameful downloads
+    if [ -z "$2" ]; then 
+
+        httpResponse=$(curl --fail --location --write-out %{http_code} --max-redirs 3 --retry 3 --retry-connrefused --retry-delay 2 --max-time 90 --url $1 --remote-name)
+        local exitCode=$?
+
+    else
+
+        httpResponse=$(curl --fail --location --write-out %{http_code} --max-redirs 3 --retry 3 --retry-connrefused --retry-delay 2 --max-time 90 --url $1 --output $2)
+        local exitCode=$?
+
+    fi
+
+    if [ $exitCode -ne 0 ] || [ $httpResponse -ne 200 ]; then
+
+        echo "Download failed with exitCode:$exitCode and httpResponse:$httpResponse"
+        return 1
+        
+    fi
+
+    return 0
+
+}
+
+function createSwapFile() {
+
+    # 1:filename, 2:megabytes, 3:path
+
+    # if path is given, know how to handle it when creating swap
+    if [ ! -z $3 ]; then
+
+        if [ -d $3 ]; then
+
+            # path exists
+            createDir=0
+
+        else
+
+            # new path
+            createDir=1
+
+        fi
+
+    else
+
+        # path not given
+        createDir=2
+
+    fi
+
+    if [ -z $1 ]; then
+
+        # (>&2 echo "error: no filename given to swap file")
+        echo "error: no filename given to swap file" | log
+        return 1
+
+    fi
+
+    if [[ $createDir -eq 2 && -e $1 ]] || [[ $createDir -eq 0 && -e "$3/$1" ]]; then
+
+        echo "error: a file with this name already exists" | log
+        return 1
+
+    fi
+
+    if [ -z $2 ]; then
+
+        echo "error: swap size (in MB) must be provided" | log
+        return 1
+
+    fi
+
+    if [[ $2 =~ '^[0-9]+$' ]] || [[ $2 -le 0 ]]; then
+
+        echo "error: swap size must be a number greater than 0" | log
+        return 1
+
+    fi
+
+    diskSizeMb=`df --output=avail -m "$PWD" | sed '1d;s/[^0-9]//g'`
+    swapSizeAllowed=$((diskSizeMb/2))
+
+    if [ $2 -gt $swapSizeAllowed ]; then
+
+        echo "error: maximum swap size (in MB) can be $swapSizeAllowed" | log
+        return 1
+
+    fi
+
+    # create swap in existing directory
+    if [ $createDir -eq 0 ]; then
+
+        swapFile="$3/$1"
+
+    fi
+
+    # create new directory for swap
+    if [ $createDir -eq 1 ]; then
+
+        mkdir -p $3
+        swapFile="$3/$1"
+
+    fi
+
+    # create swap in current path
+    if [ $createDir -eq 2 ]; then
+
+        swapFile="$1"
+
+    fi
+
+    # generate swap file and mount it
+    dd if=/dev/zero of=$swapFile bs=1M count=$2
+    mkswap $swapFile >/dev/null || { echo 'mkswap failed' ; return 1; }
+    swapon $swapFile >/dev/null || { echo 'swapon failed' ; return 1; }
+    chmod 600 $swapFile >/dev/null || { echo 'chmod swapfile failed' ; return 1; }
+
+    if [ ! -e $swapFile ]; then
+
+        echo "error: did not complete swap creation properly"
+        return 1
+
+    fi
+
+    echo "$swapFile"
+    return 0
+
+}
+
+function removeSwapFile() {
+
+    # 1: filename given when created swap with createSwapFile()
+    if [ ! -e $1 ]; then
+
+        echo "error: a swapfile with this name was not found. did nothing" | log
+        return 1
+
+    fi
+
+    swapoff $1
+    rm -f $1
+
+    return 0
 
 }
 
