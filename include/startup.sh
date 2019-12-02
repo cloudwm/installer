661812edf8de16efb8970262853d85dcf9c4ebab
Nadav Gordon <nadavg@omc.co.il>
2019-12-02 15:10:45 +0200

    mod: moved install log to root folder

--- include/startup.sh
+++ include/startup.sh
@@ -44,18 +44,11 @@ function checkTempDir() {
 
 function log() {
 
-    rootDir=$(rootDir)
     logScriptName=$(basename $0)
 
-    if [ -z "$logDir" ]; then
+    if [ ! -d "$CWM_LOGDIR" ]; then
 
-	    logDir="$rootDir/temp"
-
-    fi
-
-    if [ ! -d "$logDir" ]; then
-
-	    mkdir -p $logDir
+	    mkdir -p $CWM_LOGDIR
 
     fi
 
@@ -63,7 +56,7 @@ function log() {
 
         printf '[%s] %s: %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$logScriptName" "$line";
 
-    done | tee -a $logDir/$(date '+%Y-%m-%d').log ${1:+${CWM_ERRORFILE}}
+    done | tee -a $CWM_LOGDIR/$(date '+%Y-%m-%d').log ${1:+${CWM_ERRORFILE}}
 
 }
 
