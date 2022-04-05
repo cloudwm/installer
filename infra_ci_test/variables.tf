5dc79ee1d24891d9148b8c223110744d6bd78176
Yuri Tsuprun <yurit@omc.co.il>
2022-04-05 07:44:32 +0300

    Terraform CI set to manual

--- infra_ci_test/variables.tf
+++ infra_ci_test/variables.tf
@@ -16,6 +16,7 @@ variable "password" {
 variable "datacenter" {
   description = "Datcenter ID's"
   type        = list(string)
+  # default     = ["IL-PT", "IL-TA"]
   default     = ["IL", "IL-PT", "IL-RH", "IL-TA", "IL-HA"]
 }
 
@@ -36,7 +37,7 @@ variable "image_id" {
     {
       name = "ci-test-rancher2.6.2"
       id   = "6000C2902374d7b52d94b0ec5361f16a"
-      ram  = 2048
+      ram  = 4096
     },
     {
       name = "ci-test-mediawiki1.3x"
@@ -58,11 +59,11 @@ variable "image_id" {
       id   = "6000C2902374d7b52d94b0ec5361f16a"
       ram  = 2048
     },
-    {
-      name = "ci-test-ispconfig3.2"
-      id   = "6000C290c0cfbc31d16be7aa76d0f7a1"
-      ram  = 2048
-    },
+    # {
+    #   name = "ci-test-ispconfig3.2"
+    #   id   = "6000C290c0cfbc31d16be7aa76d0f7a1"
+    #   ram  = 2048
+    # },
     {
       name = "ci-test-ansible-awx"
       id   = "6000C290f479e48dff4c75280c8c6c5e"
@@ -98,14 +99,54 @@ variable "image_id" {
       id   = "6000C291d5f701ac8ab70696ac3cdebd"
       ram  = 2048
     },
-    {
-      name = "ci-test-plesk"
-      id   = "6000C291f31096639849b90df58998fa"
-      ram  = 2048
-    },
+    # {
+    #   name = "ci-test-plesk"
+    #   id   = "6000C291f31096639849b90df58998fa"
+    #   ram  = 2048
+    # },
     {
       name = "ci-test-lamp"
       id   = "6000C2920688a13c0d09e2416d085793"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-rubyonrails6.1.3"
+      id   = "6000C29f7d752d83e3a6038fb055372b"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-memcached"
+      id   = "6000C29f5acf0dcb44e3fada267392f8"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-grafana"
+      id   = "6000C29f147b461fb9f9790c867829c4"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-pfsense2.5.2"
+      id   = "6000C29dc443468c8ccdda2160f3fea7"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-mysql8"
+      id   = "6000C29e9c071b118704b98cb613d888"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-phpfpm7.4"
+      id   = "6000C29e6348244f9a2e9ca72a89d9a2"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-odoo14"
+      id   = "6000C29e3748c8ca3a758a721f2b4cab"
+      ram  = 2048
+    },
+    {
+      name = "ci-test-zentyal7"
+      id   = "6000C29e328aaffac3e3bff1c7aabd64"
       ram  = 2048
     }
   ]
