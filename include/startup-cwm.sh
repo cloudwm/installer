def423e6edaa4ed08c68ae1c590cc89446fef66f
Oren Baranovsky <orenb@omc.co.il>
2022-09-08 12:39:42 +0300

    edited startup-cwm for key issue testing

--- include/startup-cwm.sh
+++ include/startup-cwm.sh
@@ -232,6 +232,7 @@ if [ ! -f "$rootDir/temp/globals-set.success" ]; then
         random_pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 14 ; echo '')
         random_num=$(shuf -i 0-50 -n1)
         export ADMINPASSWORD=${random_pass}${random_num}
+        echo "ADMINPASSWORD is: ${ADMINPASSWORD}"
     else
         export ADMINPASSWORD="$CWM_PASSWORD"
     fi 
