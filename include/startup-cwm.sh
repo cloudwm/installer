978c358cd11c86b5085cfbcfc834291f33df3f18
Lior97 <lioryasharzada@gmail.com>
2026-02-16 14:20:22 +0200

    [Change] domain naming

--- include/startup-cwm.sh
+++ include/startup-cwm.sh
@@ -248,7 +248,7 @@ if [ ! -f "$rootDir/temp/globals-set.success" ]; then
     export CWM_SERVERIP="$(getServerIP)"
 
     CONFIG_FILE="/root/guest.conf"
-    export CWM_DOMAIN=$(grep -E '^[[:space:]]*cwm_domain0[[:space:]]*=' "${CONFIG_FILE}" \
+    export CWM_DOMAIN=$(grep -E '^[[:space:]]*fqdn0[[:space:]]*=' "${CONFIG_FILE}" \
          | sed -E 's/^[[:space:]]*cwm_domain0[[:space:]]*=[[:space:]]*//; s/["'\'']//g; s/[[:space:]]*$//' \
          | head -n 1)
 
